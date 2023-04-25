3e815f2af33389579a9bbb255b6c7dec977012e6
olga-bylnova <olga.bylnova@yandex.by>
2023-04-25 17:01:00 +0300

    add uri constants

---
+++ src/main/scala/com/innowise/util/ApiConstant.scala
@@ -0,0 +1,7 @@
+package com.innowise.util
+
+object ApiConstant {
+  val CASES_BY_COUNTRY_REQUEST_URI = "https://api.covid19api.com/country/%s/status/confirmed?from=%s&to=%s"
+  val ALL_COUNTRIES_URI = "https://api.covid19api.com/countries"
+
+}
